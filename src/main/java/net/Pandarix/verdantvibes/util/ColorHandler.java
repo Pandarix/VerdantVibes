c9c339317da11a5e97bed1d2685be1f8c3584b2c
Ruven <ruven@uni-bremen.de>
2023-09-23 18:01:22 +0200

    flowerpatches and monsteras made tintable

--- src/main/java/net/Pandarix/verdantvibes/util/ColorHandler.java
+++ src/main/java/net/Pandarix/verdantvibes/util/ColorHandler.java
@@ -17,7 +17,7 @@ public class ColorHandler
         //Foliage Coloring
         event.getBlockColors().register((state, world, pos, tintIndex) ->
                         world != null && pos != null ? BiomeColors.getAverageFoliageColor(world, pos) : FoliageColor.getDefaultColor(),
-                BlockInit.PARLOUR_PALM.get(), BlockInit.DRAGON_TREE.get(), BlockInit.MONSTERA.get());
+                BlockInit.PARLOUR_PALM.get(), BlockInit.DRAGON_TREE.get(), BlockInit.MONSTERA.get(), BlockInit.CANDY_TUFT.get(), BlockInit.PERIWINKLE.get(), BlockInit.LOBELIA.get());
     }
 
     /*
