8362faec5589f8171610d6b19d884131b86811e5
Ruven <ruven@uni-bremen.de>
2023-11-26 20:47:04 +0100

    fixed #7

--- src/main/java/net/Pandarix/verdantvibes/particles/ParticleFactoryInit.java
+++ src/main/java/net/Pandarix/verdantvibes/particles/ParticleFactoryInit.java
@@ -2,6 +2,8 @@ package net.Pandarix.verdantvibes.particles;
 
 import net.Pandarix.verdantvibes.VerdantVibes;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -9,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = VerdantVibes.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ParticleFactoryInit {
     @SubscribeEvent
+    @OnlyIn(Dist.CLIENT)
     public static void registerParticleFactories(final RegisterParticleProvidersEvent event) {
         Minecraft.getInstance().particleEngine.register(ParticleInit.BURNWEED_PARTICLES.get(),
                 BurnweedParticles.Provider::new);
